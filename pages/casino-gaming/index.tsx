859c6de4c3d52a5cdb2c30b2953e660a1ce5458a
Darren White <dwhite@gentingcasinos.co.uk>
2021-12-23 18:59:21 +0000

    refactor: :recycle: Casino Gaming Test

---
+++ pages/casino-gaming/index.tsx
@@ -0,0 +1,15 @@
+
+import tw from 'twin.macro'
+
+
+export default function CasinoGaming() {
+	return (
+		<div className='flex flex-col justify-between'>			
+
+			<main tw='overflow-x-hidden'>
+				Casino Gaming Test
+			</main>
+		</div>
+	)
+}
+
